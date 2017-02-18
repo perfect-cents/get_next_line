e103e562e2066fb9ce773266a06ad8e35a995551
Braden Pezeshki <braden.pezeshki@gmail.com>
2017-02-17 17:31:02 -0800

    fixed some things

--- get_next_line.c
+++ get_next_line.c
@@ -1,39 +1,83 @@
-void		gnl_reader()
-{}
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpezeshk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/09 11:05:59 by bpezeshk          #+#    #+#             */
+/*   Updated: 2017/02/09 11:06:08 by bpezeshk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-t_gnldata	*get_curr(t_list *gnllst, const int fd)
+#include "get_next_line.h"
+#include <libft.h>
+
+static t_list	*get_curr(t_list **lst, const int fd)
 {
-	t_gnldata	*data;
+	t_list	*el;
 
-	while (gnllst)
+	el = *lst;
+	while (el)
 	{
-		if (gnllst->fd == fd)
-			return (gnllst->content);
-		gnllst = gnllst->next;
+		if ((int)el->content_size == fd)
+			return (el);
+		el = el->next;
 	}
-	if ((data = (t_gnldata *)ft_memalloc(sizeof(*data))) == NULL)
-		return (NULL)
-			;
-	data->fd = fd;
-	data->prog = NULL;
-	data->psiz = 0;
-	ft_lstpush(&gnllst, ft_lstnew(data, sizeof(data)));
-	return (data);
+	if (!(el = ft_lstnew("", 1)))
+		return (NULL);
+	el->content_size = fd;
+	ft_lstadd(lst, el);
+	return (*lst);
 }
 
-int		get_next_line(const int fd, char **line)
+static int		setnxtline(t_list *curr, char **line)
 {
-	int				status;
-	const char		buf[BUFF_SIZE];
-	t_list			gnllst;
-	t_gnldata		*curr;
+	char	*p1;
+	char	*p2;
 
-	curr = get_curr(fd);
-
-	while ((status = read(fd, buf, BUFF_SIZE)) > 0)
-	{
-		byte_c += status;
-		(char *)memjoin(*line, byte_c, buf, BUFF_SIZE);
-	}
+	p1 = curr->content;
+	if (!*p1)
+		return ((int)(*line = NULL));
+	p2 = curr->content;
+	while (*p2 != '\n' && *p2)
+		p2++;
+	if (!(*line = ft_strndup(p1, p2 - p1)) \
+	|| !(curr->content = *p2 ? ft_strdup(p2 + 1) : ""))
+		return (-1);
+	free(p1);
 	return (1);
+}
+
+static int		read_in(t_list *curr)
+{
+	char	*tmp;
+	char	buf[BUFF_SIZE + 1];
+	int		status;
+
+	if (ft_strchr(curr->content, '\n'))
+		return (0);
+	while ((status = read(curr->content_size, buf, BUFF_SIZE)) > 0)
+	{
+		buf[status] = '\0';
+		tmp = curr->content;
+		if (!(curr->content = ft_strjoin(tmp, buf)))
+			return (-1);
+		free(tmp);
+		if (ft_strchr(buf, '\n'))
+			return (1);
+	}
+	return (status);
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	static t_list	*lst;
+	t_list			*curr;
+
+	if (!line)
+		return (-1);
+	if (fd < 0 || !(curr = get_curr(&lst, fd)) || read_in(curr) < 0)
+		return ((int)(*line = NULL) - 1);
+	return (setnxtline(curr, line));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,21 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpezeshk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/09 14:25:44 by bpezeshk          #+#    #+#             */
+/*   Updated: 2017/02/09 14:25:50 by bpezeshk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdlib.h>
-# include <string.h>
-# include <unistd.h>
-# include <fcntl.h>
-# include "libft/libft.h"
+# define BUFF_SIZE 100
 
-#define BUFF_SIZE 32
-
-typedef struct
-{
-	const int	fd;
-	void		*prog;
-	int			psiz;
-}				t_gnldata;
-
-int						get_next_line(const int fd, char **line);
+int get_next_line(const int fd, char **line);
 
 #endif
